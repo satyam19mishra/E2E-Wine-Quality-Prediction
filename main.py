03f29e4ad6918cc157bea54da2c2bb16975c083e
Satya saurabh Mishra <103360091+satyam19mishra@users.noreply.github.com>
2024-01-28 14:56:56 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from wineQuality import logger
+from wineQuality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome to india ")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
