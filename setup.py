3ddece45381dff6666316d778786d154c2b62fce
Satya saurabh Mishra <103360091+satyam19mishra@users.noreply.github.com>
2024-01-28 13:08:34 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "E2E-Wine-Quality-Prediction"
+AUTHOR_USER_NAME = "satyam19mishra"
+SRC_REPO = "E2E-Wine-Quality-Prediction"
+AUTHOR_EMAIL = "samrewa19@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for wine quality app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
